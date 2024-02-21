07d11fa0b3b62d415120ea7c88f6e3611ccac0db
abdrhmnjgrdhr <abdurrahamanjagirdhar2412@gmail.com>
2024-02-21 20:31:11 +0530

    changes

--- src/Components/Navbar/Cart.jsx
+++ src/Components/Navbar/Cart.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import { FaCartPlus } from 'react-icons/fa6'
+import React from "react";
+import { FaCartPlus } from "react-icons/fa6";
 
 function Cart() {
+  var number = 4;
   return (
     <div>
-      <FaCartPlus className="text-2xl text-green-500 " />
+      <div className="relative">
+        <div>
+          <FaCartPlus className="text-2xl text-green-500 " />
+        </div>
+        <div className="absolute w-[15px] h-[15px] bg-red-400 rounded-full flex items-center justify-center text-white -top-1 -right-2">
+          <p className="text-xs ">{number}</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
--- src/Components/Navbar/Favorite.jsx
+++ src/Components/Navbar/Favorite.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 import { MdOutlineFavoriteBorder } from "react-icons/md";
 function Favorite() {
+  var number = 5;
   return (
     <div>
-                <MdOutlineFavoriteBorder className="text-2xl text-green-500 " />
+      <div className="relative">
+        <div>
+          <MdOutlineFavoriteBorder className="text-2xl text-green-500 " />
+        </div>
+        <div className="absolute w-[15px] h-[15px] bg-red-400 rounded-full flex items-center justify-center text-white -top-1 -right-2">
+          <p className="text-xs ">{number}</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Favorite
+export default Favorite;
